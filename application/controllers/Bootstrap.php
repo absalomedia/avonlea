8cbd4197e6c0dc7a457001c267f80b6411f4a640
Lawrence <media@absalom.biz>
2016-05-31 15:45:12 +1000

    Whoops handler

--- application/controllers/Bootstrap.php
+++ application/controllers/Bootstrap.php
@@ -13,6 +13,19 @@ class Bootstrap extends CI_Controller
         $run     = new Run();
         $handler = new PrettyPageHandler();
 
+        $run->pushHandler($handler);
+// Example: tag all frames inside a function with their function name
+        $run->pushHandler(function ($exception, $inspector, $run) {
+            $inspector->getFrames()->map(function ($frame) {
+                if ($function = $frame->getFunction()) {
+                    $frame->addComment("This frame is within function '$function'", 'cpt-obvious');
+                }
+                return $frame;
+            });
+        });
+        $run->register();
+
+
         $this->load->helper('url');
         if (!file_exists(APPPATH.'/config/database.php')) {
             redirect('install');
