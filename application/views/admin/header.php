a31aae31736ce67c0af32494723a90b30ddb9b8b
Lawrence <media@absalom.biz>
2016-07-08 15:11:08 +1000

    Admin menu alpha

--- application/views/admin/header.php
+++ application/views/admin/header.php
@@ -58,68 +58,69 @@ if (isset($additional_header_info)) {
     <div id="layout">
     <!-- Menu toggle -->
 
-    <nav role="navigation" id="foo" class="nav-collapse">
-        <a class="pure-menu-heading" href="<?php echo site_url('admin'); ?>">Avonlea</a>
+        <a href="#menu" id="menuLink" class="menu-link">
+        <!-- Hamburger icon -->
+        <span></span>
+        </a>
 
-            <ul>
-
-                    <li class="dropdown">
-                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false"><?php echo lang('common_sales'); ?> <span class="caret"></span></a>
-                        <ul class="dropdown-menu" role="menu">
-                            <li><a href="<?php echo site_url('admin/orders'); ?>"><?php echo lang('common_orders'); ?></a></li>
+    <div id="menu">
+        <div class="pure-menu">
+            <a class="pure-menu-heading" href="#" class="pure-menu-link"><span class="fa fa-cogs"> Avonlea</span></a>
+            <ul class="pure-menu-list">
+                        <li class="pure-menu-item"><a href="<?php echo site_url('admin/orders'); ?>"><?php echo lang('common_orders'); ?> <span class="caret"></span></a>
                             <?php if (CI::auth()->checkAccess('Admin')) : ?>
-                                <li><a href="<?php echo site_url('admin/customers'); ?>"><?php echo lang('common_customers'); ?></a></li>
-                                <li><a href="<?php echo site_url('admin/customers/groups'); ?>"><?php echo lang('common_groups'); ?></a></li>
-                                <li><a href="<?php echo site_url('admin/reports'); ?>"><?php echo lang('common_reports'); ?></a></li>
-                                <li><a href="<?php echo site_url('admin/coupons'); ?>"><?php echo lang('common_coupons'); ?></a></li>
-                                <li><a href="<?php echo site_url('admin/gift-cards'); ?>"><?php echo lang('common_gift_cards'); ?></a></li>
+                            <ul class="pure-menu-list">
+                                <li class="pure-menu-item"><a href="<?php echo site_url('admin/customers'); ?>" class="pure-menu-link"><?php echo lang('common_customers'); ?></a></li>
+                                <li class="pure-menu-item"><a href="<?php echo site_url('admin/customers/groups'); ?>" class="pure-menu-link"><?php echo lang('common_groups'); ?></a></li>
+                                <li class="pure-menu-item"><a href="<?php echo site_url('admin/reports'); ?>" class="pure-menu-link"><?php echo lang('common_reports'); ?></a></li>
+                                <li class="pure-menu-item"><a href="<?php echo site_url('admin/coupons'); ?>" class="pure-menu-link"><?php echo lang('common_coupons'); ?></a></li>
+                                <li class="pure-menu-item"><a href="<?php echo site_url('admin/gift-cards'); ?>" class="pure-menu-link"><?php echo lang('common_gift_cards'); ?></a></li>
+                            </ul>
+                        </li>
                             <?php endif; ?>
-                        </ul>
-                    </li>
-<?php if (CI::auth()->checkAccess('Admin')) : ?>
-                    <li class="dropdown">
-                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false"><?php echo lang('common_catalog'); ?> <span class="caret"></span></a>
-                        <ul class="dropdown-menu" role="menu">
-                            <li><a href="<?php echo site_url('admin/categories'); ?>"><?php echo lang('common_categories'); ?></a></li>
-                            <li><a href="<?php echo site_url('admin/products'); ?>"><?php echo lang('common_products'); ?></a></li>
-                            <li><a href="<?php echo site_url('admin/digital_products'); ?>"><?php echo lang('common_digital_products'); ?></a></li>
+                    <?php if (CI::auth()->checkAccess('Admin')) : ?>
+                    <li class="pure-menu-item">
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false"><?php echo lang('common_catalog'); ?> <span class="caret"></span></a></li>
+                        <ul class="pure-menu-list">
+                            <li class="pure-menu-item"><a href="<?php echo site_url('admin/categories'); ?>" class="pure-menu-link"><?php echo lang('common_categories'); ?></a></li>
+                            <li class="pure-menu-item"><a href="<?php echo site_url('admin/products'); ?>" class="pure-menu-link"><?php echo lang('common_products'); ?></a></li>
+                            <li class="pure-menu-item"><a href="<?php echo site_url('admin/digital_products'); ?>" class="pure-menu-link"><?php echo lang('common_digital_products'); ?></a></li>
                         </ul>
                     </li>
 
-                    <li class="dropdown">
+                    <li class="pure-menu-item">
                         <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false"><?php echo lang('common_content'); ?> <span class="caret"></span></a>
-                        <ul class="dropdown-menu" role="menu">
-                            <li><a href="<?php echo site_url('admin/banners'); ?>"><?php echo lang('common_banners'); ?></a></li>
-                            <li><a href="<?php echo site_url('admin/pages'); ?>"><?php echo lang('common_pages'); ?></a></li>
+                        <ul class="pure-menu-list">
+                            <li class="pure-menu-link"><a href="<?php echo site_url('admin/banners'); ?>" class="pure-menu-link"><?php echo lang('common_banners'); ?></a></li>
+                            <li class="pure-menu-link"><a href="<?php echo site_url('admin/pages'); ?>" class="pure-menu-link"><?php echo lang('common_pages'); ?></a></li>
                         </ul>
                     </li>
 
-                    <li class="dropdown">
+                    <li class="pure-menu-item">
                         <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false"><?php echo lang('common_administrative'); ?> <span class="caret"></span></a>
-                        <ul class="dropdown-menu" role="menu">
-                            <li><a href="<?php echo site_url('admin/settings'); ?>"><?php echo lang('common_Avonlea_configuration'); ?></a></li>
-                            <li><a href="<?php echo site_url('admin/shipping'); ?>"><?php echo lang('common_shipping_modules'); ?></a></li>
-                            <li><a href="<?php echo site_url('admin/payments'); ?>"><?php echo lang('common_payment_modules'); ?></a></li>
-                            <li><a href="<?php echo site_url('admin/settings/canned_messages'); ?>"><?php echo lang('common_canned_messages'); ?></a></li>
-                            <li><a href="<?php echo site_url('admin/locations'); ?>"><?php echo lang('common_locations'); ?></a></li>
-                            <li><a href="<?php echo site_url('admin/users'); ?>"><?php echo lang('common_administrators'); ?></a></li>
-                            <li><a href="<?php echo site_url('admin/sitemap'); ?>"><?php echo 'Sitemap'; ?></a></li>
+                        <ul class="pure-menu-list">
+                            <li class="pure-menu-link"><a href="<?php echo site_url('admin/settings'); ?>" class="pure-menu-link"><?php echo lang('common_Avonlea_configuration'); ?></a></li>
+                            <li class="pure-menu-link"><a href="<?php echo site_url('admin/shipping'); ?>" class="pure-menu-link"><?php echo lang('common_shipping_modules'); ?></a></li>
+                            <li class="pure-menu-link"><a href="<?php echo site_url('admin/payments'); ?>" class="pure-menu-link"><?php echo lang('common_payment_modules'); ?></a></li>
+                            <li class="pure-menu-link"><a href="<?php echo site_url('admin/settings/canned_messages'); ?>" class="pure-menu-link"><?php echo lang('common_canned_messages'); ?></a></li>
+                            <li class="pure-menu-link"><a href="<?php echo site_url('admin/locations'); ?>" class="pure-menu-link"><?php echo lang('common_locations'); ?></a></li>
+                            <li class="pure-menu-link"><a href="<?php echo site_url('admin/users'); ?>" class="pure-menu-link"><?php echo lang('common_administrators'); ?></a></li>
+                            <li class="pure-menu-link"<a href="<?php echo site_url('admin/sitemap'); ?>" class="pure-menu-link"><?php echo 'Sitemap'; ?></a></li>
                         </ul>
                     </li>
                     <?php endif; ?>
 
-                    <li class="dropdown">
+                    <li class="pure-menu-item">
                         <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false"><?php echo lang('common_actions'); ?> <span class="caret"></span></a>
-                        <ul class="dropdown-menu" role="menu">
-                            <li><a href="<?php echo site_url('admin/dashboard'); ?>"><?php echo lang('common_dashboard'); ?></a></li>
-                            <li><a href="<?php echo site_url(); ?>"><?php echo lang('common_front_end'); ?></a></li>
-                            <li><a href="<?php echo site_url('admin/logout'); ?>"><?php echo lang('common_log_out'); ?></a></li>
+                        <ul class="pure-menu-list">
+                            <li class="pure-menu-link"><a href="<?php echo site_url('admin/dashboard'); ?>" class="pure-menu-link"><?php echo lang('common_dashboard'); ?></a></li>
+                            <li class="pure-menu-link"><a href="<?php echo site_url(); ?>" class="pure-menu-link"><?php echo lang('common_front_end'); ?></a></li>
+                            <li class="pure-menu-link"><a href="<?php echo site_url('admin/logout'); ?>" class="pure-menu-link"><?php echo lang('common_log_out'); ?></a></li>
                         </ul>
                     </li>
-
-            </ul>
-        </div>
-    </nav>
+                </ul>
+                </div>
+            </div>
 <?php endif; ?>
 <div class="cover-image manager <?php if (CI::auth()->isLoggedIn(false, false)) :
 ?>logged<?php
