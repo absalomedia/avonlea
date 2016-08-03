614d117d71c309ef7fbd82622b2d747135c1dbdf
Lawrence <media@absalom.biz>
2016-08-03 11:30:07 +1000

    Favicon stack

--- themes/default/views/header.php
+++ themes/default/views/header.php
@@ -17,6 +17,14 @@ echo config_item('company_name'); ?></title>
 
 <link href='https://fonts.googleapis.com/css?family=Comfortaa|Raleway' rel='stylesheet' type='text/css'>
 
+
+<link rel="apple-touch-icon" sizes="180x180" href="img/apple-touch-icon.png">
+<link rel="icon" type="image/png" href="img/favicon-32x32.png" sizes="32x32">
+<link rel="icon" type="image/png" href="img/favicon-16x16.png" sizes="16x16">
+<link rel="manifest" href="img/manifest.json">
+<link rel="mask-icon" href="img/safari-pinned-tab.svg" color="#5bbad5">
+<meta name="theme-color" content="#ffffff">
+
 <?php
 $_css = new CSSCrunch();
 
